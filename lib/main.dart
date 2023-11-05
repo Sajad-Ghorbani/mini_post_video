68239a43e5e5415d59a8e8478b49d6d8ff119610
sajad ghorbani <sajad.2012.gh@gmail.com>
2023-11-05 18:59:09 +0330

    feat: change to get material app and add services

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mini_post_video/app/config/routes/app_pages.dart';
+import 'package:mini_post_video/app/core/service/services.dart';
+import 'package:mini_post_video/splash_screen.dart';
 
-void main() {
+void main()async {
+  await initServices();
   runApp(const MyApp());
+}
+
+Future<void> initServices() async {
+  await Get.putAsync(() => AppServices().init());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,22 +18,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      getPages: AppPages.pages,
+      initialRoute: Routes.initial,
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
   }
 }
